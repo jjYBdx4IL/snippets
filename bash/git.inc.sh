1d0d5ed2518637390f8c06a1b403736b38357095
jjYBdx4IL <jjYBdx4IL@github.com>
2017-08-25 05:49:49 +0200

    git fsck all

--- bash/git.inc.sh
+++ bash/git.inc.sh
@@ -1,16 +1,27 @@
-# vim:set syntax=sh et sw=4:
+# vim:set sw=4 ts=4 et ai smartindent fileformat=unix fileencoding=utf-8 syntax=sh:
 
-_git_pull_all() {
-	local pwd=$(pwd)
+_git_fsck_all() {
+    local pwd=$(pwd)
+    set -Ee
 	for f in $pwd/*; do
 	    if ! test -d $f/.git; then continue; fi
-		echo -n "$f: "
+		echo "$f: "
 		cd $f
-		git pull --all
+		LC_ALL=C git fsck --full --strict
 	done
 	cd $pwd
 }
 
+_git_pull_all() {
+    local pwd=$(pwd)
+	for f in $pwd/*; do
+	    if ! test -d $f/.git; then continue; fi
+		echo -n "$f: "
+		cd $f
+		LC_ALL=C git pull --all
+	done
+	cd $pwd
+}
 
 _git_status_all() {
 	local okmd5="d158955c3de1219dbdee7368efbfd46c"
@@ -31,7 +42,6 @@ _git_status_all() {
 	done
 	cd $pwd
 }
-
 
 _git_summary () 
 { 
