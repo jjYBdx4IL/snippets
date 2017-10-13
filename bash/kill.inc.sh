0de57755826ae5a21c5956b85b563eee2a297df0
jjYBdx4IL <jjYBdx4IL@github.com>
2017-10-13 05:30:52 +0200

    kill cmds

--- bash/kill.inc.sh
+++ bash/kill.inc.sh
@@ -5,3 +5,8 @@ _kill_childs_grep() {
     ps -o pid,cmd | grep $1 | grep -v grep
 }
 
+_kill_by_binary_and_port() {
+    local pids=`netstat -tlpen 2>/dev/null | grep "^tcp.* .*:$2 .*\/$1 *$" | sed -e 's:.*\b\([0-9][0-9]*\)/[^\/]*\b.*:\1:'`
+    kill -HUP $pids
+}
+
